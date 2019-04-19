c30dd90c5c134bc4c92f56a79271342fa5ed7509
hukuda222 <zionbanzai3@gmail.com>
2019-04-19 15:12:50 +0900

    Update makehdf5.py

--- makehdf5.py
+++ makehdf5.py
@@ -4,8 +4,8 @@ import pickle
 import tqdm
 
 index = 0
-with h5py.File("/raid/fujisyo/java-large/java-large-train.h5", "w") as h5f:
-    with open("/raid/fujisyo/java-large/java-large.train.c2s", "r") as cf:
+with h5py.File("train.h5", "w") as h5f:
+    with open("train.c2s", "r") as cf:
         for line in tqdm.tqdm(cf):
             h5f.create_group(str(index))
             h5f[str(index)].create_dataset('line', data=line)
