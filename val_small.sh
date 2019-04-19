8814d8468b1f31ee0416d69456ddbc09ae4145c0
hukuda222 <zionbanzai3@gmail.com>
2019-04-19 15:10:15 +0900

    Delete val_small.sh

--- val_small.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python main.py --trainpath /home/lr/fujisyo/java-small/output.h5 -g 0 --validpath /home/lr/fujisyo/java-small/output.h5  --datapath /home/lr/fujisyo/java-small/java-small.dict.c2s -e 1 -b 30 --eval --resume small0.model --trainnum 691974 --validnum 100
-#-b 450 --resume small5.model --trainnum 691974 --validnum 23844
