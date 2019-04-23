a08c5228bb1d91a5a0278cd9f47fe0a5c28a4916
hukuda222 <zionbanzai3@gmail.com>
2019-04-23 15:23:04 +0900

    Update makehdf5.py

--- makehdf5.py
+++ makehdf5.py
@@ -5,7 +5,7 @@ import tqdm
 
 index = 0
 with h5py.File("train.h5", "w") as h5f:
-    with open("train.c2s", "r") as cf:
+    with open("java-small.train.c2s", "r") as cf:
         for line in tqdm.tqdm(cf):
             h5f.create_group(str(index))
             h5f[str(index)].create_dataset('line', data=line)
